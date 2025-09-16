73e17dd4da38e516b5137e9bdde2b3bd5e3de8e0
WeerakoolBoonthung <66050392@kmitl.ac.th>
2025-09-17 00:54:18 +0700

    Add files via upload

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Weerakool"
+last_name = "Boonthung"
+whole_name = first_name + " " + last_name
+print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+Real_age = 20
+Fourty_two = 42
+my_age = Real_age + Fourty_two
+print(my_age)
